5837594396109a9b671ebca68427698b31f1dc8f
giannisvassiliou <giannis_vassiliou@yahoo.gr>
2023-05-08 11:19:43 +0300

    Add files via upload

---
+++ src/main/scala/org/iswc/ping/Constants.scala
@@ -0,0 +1,13 @@
+package org.eswc.smarter
+
+object Constants {
+
+  val RDF_TYPE = "<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>"
+  val dbpediaUri = "dbpedia"
+  val ORIGINAL_GARPH = "original"
+
+  val clusters = "_dataset/"
+  val dataset = "dataset"
+  val level = "level/"
+
+}

ea8a43cdcadf4c7023ccf64980ac688a70f2aa9a
troulin <59057867+troulin@users.noreply.github.com>
2023-05-08 12:45:18 +0300

    Update run_partitioner.sh

--- scripts/run_partitioner.sh
+++ scripts/run_partitioner.sh
@@ -1,5 +1,5 @@
 spark-submit \
---class org.ics.isl.Partitioner \
+--class org.iswc.ping.Partitioner \
 --driver-memory DRIVER_MEM \
 --executor-memory EXECUTOR_MEM \
 --conf spark.speculation=true \
